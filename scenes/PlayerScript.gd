3d35ab362e95588fc0ac9ee39ddc5c3498e17411
RafaelM4gn <rfl.magno02@gmail.com>
2023-06-20 02:44:51 -0300

    feature: update moviment system

--- scenes/PlayerScript.gd
+++ scenes/PlayerScript.gd
@@ -4,32 +4,66 @@ onready var bullet_instance = preload("res://Scenes/Bullet.tscn")
 onready var bullet2_instance = preload("res://Scenes/Bullet2.tscn")
 onready var bullet3_instance = preload("res://Scenes/Bullet3.tscn")
 
+var positions = [380, 430, 500, 570, 630]
+var current_position = 2
+var y = 500
+var player_speed = 0.1
+
+onready var tween = $Tween
+
 var motion = Vector2.ZERO;
 var speed = 200
 
-var shoot_type = 1
+var shoot_type = 3
+
+func _ready():
+	self.position = Vector2(positions[current_position], y)
 
 func _process(delta):
-	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
+	if Input.is_action_just_pressed("move_right") or Input.is_action_just_pressed("move_left"):
 		$PlayerSprite.play("turn")
-	if motion.x ==0:
+	else:
 		$PlayerSprite.play("idle")
-	
+
 	if Input.is_action_pressed("ui_right"):
-		motion.x = speed
 		$PlayerSprite.flip_h = true
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -speed
 		$PlayerSprite.flip_h = false
-	else:
-		motion.x = 0
+
+	if Input.is_action_just_pressed("move_left"):
+		move_left()
+	
+	if Input.is_action_just_pressed("move_right"):
+		move_right()
+		
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 	
 	move_and_slide(motion,Vector2.UP)
 
+
+func move_left():
+	#check if player is not moving
+	if tween.is_active():
+		return
+	elif current_position > 0:
+		current_position -= 1
+		tween.interpolate_property(self, "position", self.position, Vector2(positions[current_position], y), player_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
+
+func move_right():
+	if tween.is_active():
+		return
+	elif current_position < positions.size() - 1:
+		current_position += 1
+		tween.interpolate_property(self, "position", self.position, Vector2(positions[current_position], y), player_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
+
+	
+
 func shoot():
+	$PlayerSprite.play("attack")
 	if shoot_type >= 1 and shoot_type < 2:
 		var bullet = bullet_instance.instance()
 		bullet.direction = $PlayerShootSpawn.global_position - global_position
